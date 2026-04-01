d1ee9127db07e9865f1eb40fc3ec7547436bf9d4
toff-tofu <e8623y@gmail.com>
2026-04-01 10:23:33 -0700

    udk bor

--- socket.js
+++ socket.js
@@ -1,344 +1,160 @@
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http, { cors: { origin: "*" } });
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
-var lobbyTicks = {};
 
-/*
- ROOMS STRUCTURE:
- rooms = {
-   roomName: {
-     players: {
-       socketId: { x, y, angle }
-     }
-   }
- }
-*/
-// Use a null-prototype object to reduce prototype-pollution risks
-let rooms = Object.create(null);
-
+// =========================
+// ROOM STORE
+// =========================
+const rooms = Object.create(null);
+// rooms = {
+//   roomName: { clients: { socketId: true } }
+// }
 
 function isValidRoomName(name) {
- return (
-   typeof name === "string" &&
-   name.length > 0 &&
-   name !== "__proto__" &&
-   name !== "constructor"
- );
-}
-function intervalTick(name){
-  if(lobbyTicks[name]){
-    lobbyTicks[name].untilAuto+=100;
-    if(rooms[name]?.startingPlayers){
-    if(Object.keys(rooms[name].players).length>=2||(lobbyTicks[name].untilAuto>=15000&&Object.keys(rooms[name].players).length>=2)){
-  lobbyTicks[name].untilStart-=100;
-    }
-  }
-  if(lobbyTicks[name].untilStart<=0){
-    lobbyTicks[name].started = true;
-    if(rooms[name]?.startingPlayers){
-    lobbyTicks[name].startingPlayers = {...rooms[name].startingPlayers};
-    }
-   // console.log(lobbyTicks[name].startingPlayers)
-  }
-  if(lobbyTicks[name].started){
-    lobbyTicks[name].gameTime+=100;
-    if(lobbyTicks[name].gameTime%10000==0&&lobbyTicks[name].borderRadius>1000){
-      lobbyTicks[name].borderRadius-=250;
-    }
-  }
-  //socket.to(socket.roomName).emit("gameTick",lobbyTicks[name]);
-  }
-  for(let id in lobbyTicks){
-    if(!Object.keys(rooms).includes(id)){
-      console.log("Deleting Lobby Ticker:", id);
-      clearInterval(lobbyTicks[id]?.interval);
-      delete lobbyTicks[id];
-      console.log("Removed Lobby Ticker", id);
-    }
-  }
+  return (
+    typeof name === "string" &&
+    name.length > 0 &&
+    name !== "__proto__" &&
+    name !== "constructor"
+  );
 }
 
+// =========================
+// CALLBACKS
+// =========================
+const callbacks = {
+  onConnect: (socket) => {},
+  onJoin: (socket, room) => {},
+  onLeave: (socket, room) => {},
+  onRoomCreate: (room) => {},
+  onRoomDelete: (room) => {},
+  onMessage: (socket, room, data) => {},
+  onDisconnect: (socket) => {},
+};
+
+// =========================
+// SOCKET SETUP
+// =========================
 io.on("connection", (socket) => {
- console.log("Player connected:", socket.id);
+  callbacks.onConnect(socket);
 
+  socket.emit("rooms_list", rooms);
 
- // Send current room list
- socket.emit("rooms_list", rooms);
+  // -----------------------
+  // CREATE ROOM
+  // -----------------------
+  socket.on("create_room", (roomName) => {
+    roomName = String(roomName);
+    if (!isValidRoomName(roomName)) return;
 
+    if (!rooms[roomName]) {
+      rooms[roomName] = { clients: {} };
+      callbacks.onRoomCreate(roomName);
+    }
 
- // ------------------------------
- // PLAYER CREATES A ROOM
- // ------------------------------
- socket.on("create_room", (roomData) => {
-  roomData.name = roomData.name+"";
-  let name = roomData.name;
-  if(name!="NO ROOM"){
-  lobbyTicks[name] = {code: name, interval: setInterval(intervalTick,100,name),started:false,untilStart: 10000,startingPlayers: {},borderRadius: 5000,gameTime: 0,untilAuto: 0};
+    joinRoom(socket, roomName);
+  });
+
+  // -----------------------
+  // JOIN ROOM
+  // -----------------------
+  socket.on("join_room", (roomName) => {
+    if (!isValidRoomName(roomName)) return;
+    if (!rooms[roomName]) return;
+
+    joinRoom(socket, roomName);
+  });
+
+  // -----------------------
+  // LEAVE ROOM
+  // -----------------------
+  socket.on("leave_room", (roomName) => {
+    if (!isValidRoomName(roomName)) return;
+    leaveRoom(socket, roomName);
+  });
+
+  // -----------------------
+  // MESSAGE TO ROOM
+  // -----------------------
+  socket.on("message", ({ room, data }) => {
+    if (!isValidRoomName(room)) return;
+    if (!rooms[room]) return;
+
+    // only allow if user is in room
+    if (!rooms[room].clients[socket.id]) return;
+
+    socket.to(room).emit("message", {
+      id: socket.id,
+      room,
+      data,
+    });
+
+    callbacks.onMessage(socket, room, data);
+  });
+
+  // -----------------------
+  // DISCONNECT
+  // -----------------------
+  socket.on("disconnect", () => {
+    // leave ALL rooms this socket joined
+    for (const roomName in rooms) {
+      if (rooms[roomName].clients[socket.id]) {
+        leaveRoom(socket, roomName);
+      }
+    }
+
+    callbacks.onDisconnect(socket);
+  });
+
+  // =========================
+  // HELPERS
+  // =========================
+
+  function joinRoom(socket, roomName) {
+    // prevent duplicate joins
+    if (rooms[roomName].clients[socket.id]) return;
+
+    socket.join(roomName);
+    rooms[roomName].clients[socket.id] = true;
+
+    socket.emit("joined_room", roomName);
+    socket.to(roomName).emit("user_joined", socket.id);
+
+    io.emit("rooms_list", rooms);
+
+    callbacks.onJoin(socket, roomName);
   }
-  //console.log(typeof name === "string" ,name.length > 0 ,name !== "__proto__" ,name !== "constructor",);
-   if (!isValidRoomName(roomData.name)) return;
-  // console.log("hi");
-   if (!rooms[roomData.name]) {
-     rooms[roomData.name] = { players: {}, blocks: {},type: roomData?.type ?? "custom",started: false, startingPlayers: {},worldBorder: 5000,};
-     console.log(`Room created: ${roomData.name}`);
-   }
 
+  function leaveRoom(socket, roomName) {
+    if (!rooms[roomName]) return;
+    if (!rooms[roomName].clients[socket.id]) return;
 
-   joinRoom(socket, roomData.name);
- });
+    socket.leave(roomName);
+    delete rooms[roomName].clients[socket.id];
 
+    socket.to(roomName).emit("user_left", socket.id);
 
- // ------------------------------
- // PLAYER JOINS A ROOM
- // ------------------------------
- socket.on("join_room", (roomName) => {
-   if (!isValidRoomName(roomName)) return;
-   if (!rooms[roomName]) return; // room must exist
+    // delete empty room
+    if (Object.keys(rooms[roomName].clients).length === 0) {
+      delete rooms[roomName];
+      callbacks.onRoomDelete(roomName);
+    }
 
+    io.emit("rooms_list", rooms);
 
-   joinRoom(socket, roomName);
- });
-
- socket.on("fetchTick", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-   let room = rooms[roomName];
-   if (!room) return;
-   let givenTickData = {...lobbyTicks[roomName]};
-   delete givenTickData.interval;
-   //console.log(`Sent to: ${roomName} for ${socket.id}`)
-     socket.emit("tickData", {
-       id: socket.id,
-       tickData: givenTickData,
-       roomData: room
-     });
-     //console.log(`Sent!`)
-     //console.log(`-~-~-~-~-~-~-~-~-~`)
- });
- // ------------------------------
- // PLAYER UPDATES MOVEMENT
- // ------------------------------
- socket.on("updateCursor", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("update", {
-       id: socket.id,
-       cursorData: { ...data },
-     });
-   }
- });
- socket.on("updateBlocks", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("updateBl", {
-       id: socket.id,
-       blockData: { ...data },
-     });
-   }
- });
- socket.on("updateChat", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("updateCh", {
-       id: socket.id,
-       chatData: { ...data },
-     });
-   }
- });
- socket.on("updateDamage", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("updateD", {
-       id: socket.id,
-       damageData: { ...data },
-     });
-   }
- });
- socket.on("removeBullet", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("updateRb", {
-       id: socket.id,
-       bulletData: { ...data },
-     });
-   }
- });
- socket.on("updateBullet", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-   if (room.players[socket.id]) {
-     socket.to(roomName).emit("updateB", {
-       id: socket.id,
-       bulletData: { ...data },
-     });
-   }
- });
- socket.on("updateData", (data) => {
-   let roomName = socket.roomName;
-   if (!roomName) return;
-
-
-   let room = rooms[roomName];
-   if (!room) return;
-
-
-   if (room.players[socket.id]) {
-     room.players[socket.id] = { ...data };
-     socket.to(roomName).emit("updateP", {
-       id: socket.id,
-       playerData: { ...data },
-     });
-   }
- });
- socket.on("getRooms", (data) => {
-  //console.log(socket.roomName,rooms,Object.keys(rooms));
-     socket.emit("gotRooms", {
-       id: socket.id,
-       roomData: {...rooms},
-     });
- });
-
-
- // ------------------------------
- // DISCONNECT HANDLING
- // ------------------------------
- socket.on("disconnect", () => {
-   console.log(`Player disconnected: ${socket.id}`);
-
-
-   let roomName = socket.roomName;
-   if (!roomName || !rooms[roomName]) return;
-
-
-   // Remove from room
-   delete rooms[roomName].players[socket.id];
-   socket.to(roomName).emit("removePlayer", socket.id);
-
-
-   // Delete room if empty
-   if (Object.keys(rooms[roomName].players).length === 0) {
-     delete rooms[roomName];
-     console.log(`Room deleted: ${roomName}`);
-   }
-
-
-   // Update rooms list for lobby clients
-   io.emit("rooms_list", rooms);
- });
+    callbacks.onLeave(socket, roomName);
+  }
 });
 
-
-// ==================================================
-// HELPER: Handle joining + initial state
-// ==================================================
-function joinRoom(socket, roomName) {
- // If the socket was in another room, remove it cleanly first
- //idk if this is nescesary:
- if (socket.roomName && socket.roomName !== roomName) {
-   const prev = socket.roomName;
-   try {
-     socket.leave(prev);
-   } catch (e) {}
-
-
-   if (rooms[prev] && rooms[prev].players && rooms[prev].players[socket.id]) {
-     delete rooms[prev].players[socket.id];
-     socket.to(prev).emit("removePlayer", socket.id);
-
-
-     if (Object.keys(rooms[prev].players).length === 0) {
-       delete rooms[prev];
-       console.log(`Room deleted: ${prev}`);
-     }
-    
-     io.emit("rooms_list", rooms);
-   }
- }
-
-
- //good after here
- socket.join(roomName);
- socket.roomName = roomName;
-
-
- // Create new player inside this specific room
- let addedData = {
-   x: 0,
-   y: 0,
-   cRoom: roomName,
- };
- if(!rooms[roomName].started){
- rooms[roomName].players[socket.id] = addedData;
-}
-rooms[roomName].startingPlayers[socket.id] = addedData;
-
- // Send existing players in this room to the newcomer
- socket.emit("initialState", rooms[roomName].players);
-
-
- // Tell others in this room about the newcomer
- socket.to(roomName).emit("newPlayer", {
-   id: socket.id,
-   x: 0,
-   y: 0,
-   cRoom: roomName,
- });
-
-
- // Update rooms list for lobby
- io.emit("rooms_list", rooms);
-
-
- console.log(`Player ${socket.id} joined room ${roomName}`);
-}
-
-
-http.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
+// =========================
+// START SERVER
+// =========================
+server.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
-
-
-
